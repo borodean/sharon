f84c21ae41dca400fb15163b040f6c6195f960fb
Vadym Borodin <borodean@gmail.com>
2017-02-09 23:58:14 +0200

    Support Google+ share count

--- plus.js
+++ plus.js
@@ -3,9 +3,11 @@
  * https://developers.google.com/+/web/share/#sharelink-endpoint
  */
 
+var countFactory = require('./core/count-factory');
 var hrefFactory = require('./core/href-factory');
 var shareFactory = require('./core/share-factory');
 
 var href = hrefFactory('https://plus.google.com/share');
 
 module.exports = shareFactory(href, 600, 600);
+module.exports.count = countFactory('https://share.yandex.ru/gpp.xml?url', Number);
