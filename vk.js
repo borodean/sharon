53b3a25d024b428c08c1709dc41e466136f832a5
Vadym Borodin <borodean@gmail.com>
2017-02-25 06:31:31 +0200

    Use HTTP whenever possible

--- vk.js
+++ vk.js
@@ -7,7 +7,7 @@ var countFactoryIndexed = require('./core/count-factory-indexed');
 var hrefFactory = require('./core/href-factory');
 var shareFactory = require('./core/share-factory');
 
-var href = hrefFactory('http://vk.com/share.php');
+var href = hrefFactory('https://vk.com/share.php');
 module.exports = shareFactory(href, 650, 610);
 
 var callbacks = [];
