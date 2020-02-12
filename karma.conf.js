a169c46a55e99ea191b8f0a76fe978fe66e03149
Vadym Borodin <borodean@gmail.com>
2020-02-12 02:49:57 +0200

    Remove the dangling commas

--- karma.conf.js
+++ karma.conf.js
@@ -4,31 +4,31 @@ module.exports = config => {
       base: 'SauceLabs',
       browserName: 'chrome',
       platform: 'Linux',
-      version: '26.0',
+      version: '26.0'
     },
     'SL Edge 13': {
       base: 'SauceLabs',
       browserName: 'MicrosoftEdge',
       platform: 'Windows 10',
-      version: '13.10586',
+      version: '13.10586'
     },
     'SL Firefox 4': {
       base: 'SauceLabs',
       browserName: 'firefox',
       platform: 'Linux',
-      version: '4.0',
+      version: '4.0'
     },
     'SL Internet Explorer 9': {
       base: 'SauceLabs',
       browserName: 'internet explorer',
       platform: 'Windows 7',
-      version: '9.0',
+      version: '9.0'
     },
     'SL Safari 8': {
       base: 'SauceLabs',
       browserName: 'safari',
       platform: 'OS X 10.10',
-      version: '8.0',
+      version: '8.0'
     }
   };
 
