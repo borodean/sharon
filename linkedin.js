590a916c1cd17f7f9103771d2d63955f0011cf85
Dmitry Evseev <dmitry@squads.com>
2021-06-28 19:21:32 +0300

    Update linkedin shareurl

--- linkedin.js
+++ linkedin.js
@@ -7,8 +7,8 @@ var countFactory = require('./core/count-factory');
 var hrefFactory = require('./core/href-factory');
 var shareFactory = require('./core/share-factory');
 
-var href = hrefFactory('https://www.linkedin.com/shareArticle', {
-  title: 'title'
+var href = hrefFactory('https://www.linkedin.com/sharing/share-offsite/', {
+  url: 'url'
 });
 
 module.exports = shareFactory(href, 600, 400);
