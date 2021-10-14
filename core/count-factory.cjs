568cd2de1dee890bcc8174d35594411dd4e0abbd
Vadym Borodin <borodean@gmail.com>
2021-10-14 14:04:33 +0300

    Provide default parameter name (fixes counts for buffer, facebook, pinterest, tumblr)

--- core/count-factory.cjs
+++ core/count-factory.cjs
@@ -1,6 +1,6 @@
 const jsonp = require('@borodean/jsonp');
 
-module.exports = function (base, parser, parameter) {
+module.exports = function (base, parser, parameter = 'callback') {
   return function (url, callback) {
     if (arguments.length < 2) {
       callback = url;
