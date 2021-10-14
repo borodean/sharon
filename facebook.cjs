8e764bb7c75b34aaa84abac50f892c0b59b196b6
Vadym Borodin <borodean@gmail.com>
2021-10-14 13:29:19 +0300

    Update `sharon.facebook.count` mechanics

--- facebook.cjs
+++ facebook.cjs
@@ -7,6 +7,6 @@ const href = hrefFactory('https://www.facebook.com/sharer.php', {
 });
 
 module.exports = shareFactory(href, 670, 340);
-module.exports.count = countFactory('https://graph.facebook.com/?id', (data) =>
-  data.share ? data.share.share_count : 0,
+module.exports.count = countFactory('https://graph.facebook.com/?access_token=550998712851057|a417089ca995161c0d147e76b1de4909&fields=og_object{engagement{count}}&id', (data) =>
+  data.og_object ? data.og_object.engagement.count : 0,
 );
